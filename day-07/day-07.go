09b95533aa26ea06aa249f9dc43b56f867e76912
Philip Norman <phil@propter.co.uk>
2017-12-27 22:06:10 +0000

    Day 7, part 2

--- day-07/day-07.go
+++ day-07/day-07.go
@@ -16,7 +16,40 @@ type node struct {
 }
 
 func main() {
-	fmt.Println(solve1(os.Args[1]))
+	// fmt.Println(solve1(os.Args[1]))
+	fmt.Println(solve2(os.Args[1]))
+}
+
+func solve2(input string) string {
+	nodes := getNodes(input)
+	nodeMap := makeNodeMap(nodes)
+
+	for _, n := range nodes {
+		if len(n.children) == 0 {
+			continue
+		}
+		cweight := []int{}
+		for _, name := range n.children {
+			cn := nodeMap[name]
+			cweight = append(cweight, weightOf(cn, nodeMap))
+		}
+		// This is ugly; I ought to apply the delta to the odd-one-out, but
+		// humans are good at that and this works so I just print it instead.
+		for _, w := range cweight {
+			delta := cweight[0] - w
+			if delta != 0 {
+				fmt.Println("unbalanced node:", n, "weights: ", cweight)
+				for _, name := range n.children {
+					cn := nodeMap[name]
+					fmt.Println(cn.name, cn.weight)
+				}
+				return n.name
+			}
+		}
+
+	}
+
+	return "ERR"
 }
 
 func solve1(input string) string {
@@ -57,9 +90,37 @@ func stringToNode(line string) node {
 		return node{}
 	}
 	w, _ := strconv.Atoi(sm[0][2])
+	// nil strings are "" in Go, calling Split on a nil will yield [""]
+	var cc []string
+	if len(sm[0][3]) > 0 {
+		cc = strings.Split(sm[0][3], ", ")
+	}
+
 	return node{
 		name:     sm[0][1],
 		weight:   w,
-		children: strings.Split(sm[0][3], ", "),
+		children: cc,
 	}
+}
+
+// weightOf returns the weight of the node and all its children
+func weightOf(n node, m map[string]node) int {
+	if len(n.children) == 0 {
+		return n.weight
+	}
+	cweight := 0
+	for _, c := range n.children {
+		cweight += weightOf(m[c], m)
+	}
+
+	return cweight + n.weight
+}
+
+// makeNodeMap maps node names to nodes
+func makeNodeMap(nn []node) map[string]node {
+	m := map[string]node{}
+	for _, n := range nn {
+		m[n.name] = n
+	}
+	return m
 }
