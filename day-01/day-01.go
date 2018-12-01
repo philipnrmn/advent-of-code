c7212c2181296e7efcd92aff7a97b8f124991683
Philip Norman <philip@mesosphere.io>
2018-11-30 22:05:23 -0800

    Day 1, part 2

--- day-01/day-01.go
+++ day-01/day-01.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	fmt.Println(solve1(os.Args[1]))
+	// fmt.Println(solve1(os.Args[1]))
+	fmt.Println(solve2(os.Args[1]))
 }
 
 func solve1(input string) int {
@@ -23,4 +24,34 @@ func solve1(input string) int {
 
 	}
 	return result
+}
+
+func solve2(input string) int {
+	fields := strings.Fields(input)
+	frequencies := []int{0}
+	f := 0
+	i := 0
+	for {
+		field := fields[i%len(fields)]
+		m := 1
+		if field[0] == byte('-') {
+			m = -1
+		}
+		n, _ := strconv.Atoi(field[1:])
+		f += n * m
+		if contains(frequencies, f) {
+			return f
+		}
+		frequencies = append(frequencies, f)
+		i++
+	}
+}
+
+func contains(haystack []int, needle int) bool {
+	for _, h := range haystack {
+		if needle == h {
+			return true
+		}
+	}
+	return false
 }
--- day-01/day-01_test.go
+++ day-01/day-01_test.go
@@ -13,6 +13,19 @@ func TestSolve1(t *testing.T) {
 		"-1 -2 -3":    -6,
 	}
 	for i, o := range io {
-		assert.Equal(t, solve1(i), o)
+		assert.Equal(t, o, solve1(i))
+	}
+}
+
+func TestSolve2(t *testing.T) {
+	io := map[string]int{
+		"+1 -2 +3 +1":    2,
+		"+1 -1":          0,
+		"+3 +3 +4 -2 -4": 10,
+		"-6 +3 +8 +5 -6": 5,
+		"+7 +7 -2 -7 -4": 14,
+	}
+	for i, o := range io {
+		assert.Equal(t, o, solve2(i))
 	}
 }
