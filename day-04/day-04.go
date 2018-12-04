33241e0d5b9bfdd30da4b98b4fbd2956e828543c
Philip Norman <phil@propter.co.uk>
2018-12-03 22:31:47 -0800

    Day 4, part 2

--- day-04/day-04.go
+++ day-04/day-04.go
@@ -11,7 +11,8 @@ import (
 )
 
 func main() {
-	fmt.Println(solve1(os.Args[1]))
+	//fmt.Println(solve1(os.Args[1]))
+	fmt.Println(solve2(os.Args[1]))
 }
 
 func solve1(input string) int {
@@ -78,5 +79,67 @@ func solve1(input string) int {
 		}
 	}
 	fmt.Printf("Minute %d was the most common, at %d times\n", mID, best)
+	return gID * mID
+}
+
+func solve2(input string) int {
+	records := strings.Split(input, "\n")
+
+	const elftime = "[2006-01-02 15:04]"
+
+	change := regexp.MustCompile("^(.+) Guard #(\\d+) begins shift")
+	asleep := regexp.MustCompile("^(.+) falls asleep")
+	awake := regexp.MustCompile("^(.+) wakes up")
+
+	// sort by timestamp
+	sort.Strings(records)
+	totals := map[int]int{}
+	minutes := map[int]map[int]int{}
+	for i := 0; i < 60; i++ {
+		minutes[i] = map[int]int{}
+	}
+
+	var start time.Time
+	var gID int
+	for _, r := range records {
+		switch {
+		case change.MatchString(r):
+			matches := change.FindStringSubmatch(r)
+			gID, _ = strconv.Atoi(matches[2])
+			break
+		case asleep.MatchString(r):
+			matches := asleep.FindStringSubmatch(r)
+			start, _ = time.Parse(elftime, matches[1])
+			break
+		case awake.MatchString(r):
+			matches := awake.FindStringSubmatch(r)
+			stop, _ := time.Parse(elftime, matches[1])
+			delta := int(stop.Sub(start).Minutes())
+			totals[gID] += delta
+
+			for i := 0; i < delta; i++ {
+				minutes[(int(start.Minute())+i)%60][gID]++
+			}
+			break
+		default:
+			fmt.Printf("could not parse %s\n", r)
+			break
+		}
+	}
+
+	var best int
+	var mID int
+	for i := 0; i < 60; i++ {
+		for g, _ := range totals {
+			m := minutes[i][g]
+			if m > best {
+				mID = i
+				gID = g
+				best = m
+			}
+		}
+
+	}
+	fmt.Printf("Guard #%d slept %d times during minute %d\n", gID, best, mID)
 	return gID * mID
 }
