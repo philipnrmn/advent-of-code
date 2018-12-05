9ce645cec6b3a7e29e67f5acaaac02749e362e92
Philip Norman <phil@propter.co.uk>
2018-12-04 22:15:55 -0800

    Day 5, part 2

--- day-05/day-05.go
+++ day-05/day-05.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	fmt.Println(solve1(os.Args[1]))
+	// fmt.Println(solve1(os.Args[1]))
+	fmt.Println(solve2(os.Args[1]))
 }
 
 func solve1(input string) int {
@@ -21,4 +22,32 @@ func solve1(input string) int {
 		}
 	}
 	return len(input)
+}
+
+func solve2(input string) int {
+	alphabet := []rune("abcdefghijklmnopqrstuvwxyz")
+	shortest := len(input)
+	var winning rune
+	for _, c := range alphabet {
+		inp := without(input, c)
+		res := solve1(inp)
+		if res < shortest {
+			shortest = res
+			winning = c
+		}
+	}
+	fmt.Printf("Winner is %#U with %d chars", winning, shortest)
+	return shortest
+}
+
+func without(input string, without rune) string {
+	result := []rune{}
+	lower := unicode.ToLower(without)
+	upper := unicode.ToUpper(without)
+	for _, c := range input {
+		if c != lower && c != upper {
+			result = append(result, c)
+		}
+	}
+	return string(result)
 }
--- day-05/day-05_test.go
+++ day-05/day-05_test.go
@@ -10,3 +10,9 @@ func TestSolve1(t *testing.T) {
 	o := 10
 	assert.Equal(t, o, solve1(i))
 }
+
+func TestSolve2(t *testing.T) {
+	i := `dabAcCaCBAcCcaDA`
+	o := 4
+	assert.Equal(t, o, solve2(i))
+}
