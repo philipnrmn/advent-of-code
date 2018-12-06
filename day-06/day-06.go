0bdbe7f41a295378ee25e43a8934f28c2476af17
Philip Norman <philip@mesosphere.io>
2018-12-06 00:14:01 -0800

    Day 6, part 1

---
+++ day-06/day-06.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println(solve1(os.Args[1]))
+}
+
+type coord struct {
+	cx int
+	cy int
+	id string
+}
+
+func solve1(input string) int {
+	lines := strings.Split(input, "\n")
+	coords := make([]coord, len(lines))
+	for i, l := range lines {
+		coords[i] = makeCoord(l)
+	}
+
+	top, right, bottom, left := boundsOf(coords)
+
+	grid := make([][]string, bottom)
+	for y := top; y < bottom; y++ {
+		grid[y] = make([]string, right)
+		for x := left; x < right; x++ {
+			grid[y][x] = findNearest(coords, x, y)
+		}
+		// fmt.Println(grid[y])
+	}
+
+	most := 0
+	for _, c := range coords {
+		if c.cy == top || c.cx == left || c.cy == bottom || c.cx == right {
+			continue
+		}
+
+		count := 0
+
+		for y := top; y < bottom; y++ {
+			for x := left; x < right; x++ {
+				if grid[y][x] == c.id {
+					count++
+				}
+			}
+		}
+		if count > most {
+			most = count
+		}
+	}
+
+	return most
+
+}
+
+func makeCoord(line string) coord {
+	xy := strings.Split(line, ", ")
+	x, _ := strconv.Atoi(xy[0])
+	y, _ := strconv.Atoi(xy[1])
+	return coord{cx: x, cy: y, id: fmt.Sprintf("%d,%d", x, y)}
+}
+
+func boundsOf(coords []coord) (int, int, int, int) {
+	top := coords[0].cx
+	right := coords[0].cy
+	bottom := coords[0].cx
+	left := coords[0].cy
+	for _, i := range coords {
+		switch {
+		case i.cy < top:
+			top = i.cy
+		case i.cx > right:
+			right = i.cx
+		case i.cy > bottom:
+			bottom = i.cy
+		case i.cx < left:
+			left = i.cx
+		}
+	}
+	return top, right, bottom, left
+}
+
+func findNearest(coords []coord, x int, y int) string {
+	var result string
+	var nearest int
+	var equidistant bool
+	for i, c := range coords {
+		distance := abs(c.cx-x) + abs(c.cy-y)
+		switch {
+		case i == 0:
+			fallthrough
+		case distance < nearest:
+
+			equidistant = false
+			nearest = distance
+			result = c.id
+		case distance == nearest:
+			equidistant = true
+		}
+	}
+	if equidistant {
+		return "..."
+	}
+	return result
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i * -1
+	}
+	return i
+}
---
+++ day-06/day-06_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSolve1(t *testing.T) {
+	i := `1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9`
+	o := 17
+	assert.Equal(t, o, solve1(i))
+}
