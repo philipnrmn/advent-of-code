f00aa33ae1db75388bd6afd5b8fccdc7f5fc15d8
Philip Norman <philip@mesosphere.io>
2018-12-07 22:53:50 -0800

    Day 8, part 2

--- day-08/day-08.go
+++ day-08/day-08.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	fmt.Println(solve1(os.Args[1]))
+	// fmt.Println(solve1(os.Args[1]))
+	fmt.Println(solve2(os.Args[1]))
 }
 
 type node struct {
@@ -25,6 +26,16 @@ func solve1(input string) int {
 
 	root, _ := parse(nums)
 	return sum(root)
+}
+
+func solve2(input string) int {
+	fields := strings.Fields(input)
+	nums := make([]int, len(fields))
+	for i, f := range fields {
+		nums[i], _ = strconv.Atoi(f)
+	}
+	root, _ := parse(nums)
+	return value(root)
 }
 
 func parse(input []int) (node, []int) {
@@ -51,6 +62,20 @@ func sum(n node) int {
 	}
 	for _, i := range n.metadata {
 		result += i
+	}
+	return result
+}
+
+func value(n node) int {
+	if len(n.children) == 0 {
+		return sum(n)
+	}
+	result := 0
+	for _, md := range n.metadata {
+		ref := md - 1
+		if ref >= 0 && ref < len(n.children) {
+			result += value(n.children[ref])
+		}
 	}
 	return result
 }
--- day-08/day-08_test.go
+++ day-08/day-08_test.go
@@ -10,3 +10,9 @@ func TestSolve1(t *testing.T) {
 	o := 138
 	assert.Equal(t, o, solve1(i))
 }
+
+func TestSolve2(t *testing.T) {
+	i := `2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2`
+	o := 66
+	assert.Equal(t, o, solve2(i))
+}
