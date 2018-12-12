f5eceddbb799431883ddbe0f099eb26d3d1f5aab
Philip Norman <philip@mesosphere.io>
2018-12-12 15:27:19 -0800

    Added benchmarks to solve2 out of curiosity (200x faster)

--- day-12/day-12_test.go
+++ day-12/day-12_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -48,4 +49,77 @@ func TestSolve2(t *testing.T) {
 func TestStrtob(t *testing.T) {
 	assert.Equal(t, byte(0x4), strtob("..#.."))
 	assert.Equal(t, byte(0x15), strtob("#.#.#"))
+}
+
+func BenchmarkWithoutTrim(b *testing.B) {
+	i1 := `...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #`
+	i2 := `#..#.#..##......###...###`
+	rules := make(map[byte]bool)
+	for _, s := range strings.Split(i1, "\n") {
+		pattern := strtob(s[0:5])
+		rules[pattern] = s[9] == '#'
+	}
+
+	state := make([]bool, len(i2))
+	for i, r := range i2 {
+		state[i] = r == '#'
+	}
+
+	// track the zeroth element (state will grow!)
+	zero := 0
+
+	for i := 0; i < b.N; i++ {
+		zero += 2
+		state = iterate(rules, state)
+	}
+}
+
+func BenchmarkWithTrim(b *testing.B) {
+	i1 := `...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #`
+	i2 := `#..#.#..##......###...###`
+	rules := make(map[byte]bool)
+	for _, s := range strings.Split(i1, "\n") {
+		pattern := strtob(s[0:5])
+		rules[pattern] = s[9] == '#'
+	}
+
+	state := make([]bool, len(i2))
+	for i, r := range i2 {
+		state[i] = r == '#'
+	}
+
+	// track the zeroth element (state will grow!)
+	zero := 0
+
+	for i := 0; i < b.N; i++ {
+		zero += 2
+		state = iterate(rules, state)
+		state, zero = trim(state, zero)
+	}
 }
