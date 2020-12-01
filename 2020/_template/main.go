9213600456eba0023f24b22e5afafdc894052704
Philip Norman <phil@propter.co.uk>
2020-12-01 22:55:44 +0000

    Automate boilerplate

---
+++ 2020/_template/main.go
@@ -0,0 +1,15 @@
+package main
+
+import . "../lib"
+
+func part1(input []int) (int, error) {
+	return 0, NYI
+}
+
+func part2(input []int) (int, error) {
+	return 0, NYI
+}
+
+func main() {
+	Solve(DAY_OF_MONTH, part1, part2)
+}
---
+++ 2020/_template/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+var i = []int{}
+
+func Test_part1(t *testing.T) {
+	_, err := part1(i)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_part2(t *testing.T) {
+	_, err := part2(i)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Benchmark_part1(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		part1(i)
+	}
+}
+
+func Benchmark_part2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		part2(i)
+	}
+}
