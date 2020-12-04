98d63210596b107df138f2338c0519af8004d6f3
Philip Norman <phil@propter.co.uk>
2020-12-04 09:23:20 +0000

    Day 4 part 2

--- 2020/day-04/main.go
+++ 2020/day-04/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"errors"
+	"regexp"
+	"strconv"
 	"strings"
 
 	. "../lib"
 )
+
+var hcl = regexp.MustCompile(`^#[0-9a-f]{6}$`)
+var pid = regexp.MustCompile(`^\d{9}$`)
+var hgt = regexp.MustCompile(`^(\d{2,3})(cm|in)$`)
+var ecls = map[string]bool{
+	"amb": true, "blu": true, "brn": true, "gry": true, "grn": true, "hzl": true, "oth": true,
+}
 
 func part1(input []string) (int, error) {
 	var count int
@@ -18,7 +28,49 @@ func part1(input []string) (int, error) {
 }
 
 func part2(input []string) (int, error) {
-	return 0, NYI
+	var count int
+	for _, passport := range parse(input) {
+		if len(passport) < 7 {
+			continue
+		}
+		if _, ok := passport["cid"]; len(passport) == 7 && ok {
+			continue
+		}
+		byr := toInt(passport["byr"])
+		if byr < 1920 || byr > 2002 {
+			continue
+		}
+		iyr := toInt(passport["iyr"])
+		if iyr < 2010 || iyr > 2020 {
+			continue
+		}
+		eyr := toInt(passport["eyr"])
+		if eyr < 2020 || eyr > 2030 {
+			continue
+		}
+		hgt, unit, err := parseHeight(passport["hgt"])
+		if err != nil {
+			continue
+		}
+		if unit == "cm" && (hgt < 150 || hgt > 193) {
+			continue
+		}
+		if unit == "in" && (hgt < 59 || hgt > 76) {
+			continue
+		}
+		if !hcl.MatchString(passport["hcl"]) {
+			continue
+		}
+		if !ecls[passport["ecl"]] {
+			continue
+		}
+		if !pid.MatchString(passport["pid"]) {
+			continue
+		}
+
+		count++
+	}
+	return count, nil
 }
 
 func parse(raw []string) []map[string]string {
@@ -40,7 +92,22 @@ func parse(raw []string) []map[string]string {
 	return results
 }
 
-func main() {
-	Solve(04, part1, part2)
+func parseHeight(raw string) (int, string, error) {
+	groups := hgt.FindStringSubmatch(raw)
+	if len(groups) == 0 {
+		return 0, "", errors.New("invalid height")
+	}
+	return toInt(groups[1]), groups[2], nil
+}
 
+func toInt(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func main() {
+	Solve(4, part1, part2)
 }
