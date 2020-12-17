b2ab34bbc0958c01e01816f924846a173a3c1bcd
Philip Norman <phil@propter.co.uk>
2020-12-17 23:43:18 +0000

    Day 17 template

---
+++ 2020/day-17/main.go
@@ -0,0 +1,15 @@
+package main
+
+import . "../lib"
+
+func part1(input []string) (int, error) {
+	return 0, NYI
+}
+
+func part2(input []string) (int, error) {
+	return 0, NYI
+}
+
+func main() {
+	Solve(17, part1, part2)
+}
